cc8da8f0aadaf509b0986b0891e2677f33d19fff
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-01 23:51:33 +0300

    Add project files.

---
+++ exercitiu 3 tema 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int media = 0;
+            int suma = 0;
+            int i;
+            for (i = 0; i <= n; i = i + 2)
+            {
+                suma = i + i;
+                media = suma / i;
+
+            }
+            Console.Write(media);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+
+}
